fe49a4dbbe9607698b1f21d38ae232893f59551b
Doug Day <dday@mastercontrol.com>
2019-05-13 15:55:35 -0600

    Added @ngrx for movie actions/effects/reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,14 @@ import { MovieDetailsComponent } from "./components/movie-details/movie-details.
 import { MovieListComponent } from "./components/movie-list/movie-list.component";
 import { AuthService } from "./services/auth.service";
 import { MovieService } from "./services/movie.service";
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import * as fromMovie from './movie.reducer';
+import { MovieEffects } from './movie.effects';
 
 @NgModule({
     declarations: [
@@ -16,6 +24,11 @@ import { MovieService } from "./services/movie.service";
     imports: [
         BrowserModule,
         HttpClientModule,
+        StoreModule.forRoot(reducers, { metaReducers }),
+        EffectsModule.forRoot([AppEffects]),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+        StoreModule.forFeature('movie', fromMovie.reducer),
+        EffectsModule.forFeature([MovieEffects]),
     ],
     providers: [
         AuthService,
---
+++ src/app/movie.actions.ts
@@ -0,0 +1,28 @@
+import { Action } from '@ngrx/store';
+import { NowPlayingSearchOptions, SearchResults } from './models/themoviedb';
+
+export enum MovieActionTypes {
+    SearchMovies = '@demo/movies/search',
+    SearchMoviesFailure = '@demo/movies/search/failure',
+    SearchMoviesSuccess = '@demo/movies/search/success',
+}
+
+export class SearchMovies implements Action {
+    readonly type = MovieActionTypes.SearchMovies;
+    constructor(public payload: NowPlayingSearchOptions) {}
+}
+
+export class SearchMoviesFailure implements Action {
+    readonly type = MovieActionTypes.SearchMoviesFailure;
+    constructor(public payload: any) {}
+}
+
+export class SearchMoviesSuccess implements Action {
+    readonly type = MovieActionTypes.SearchMoviesSuccess;
+    constructor(public payload: SearchResults) {}
+}
+
+export type MovieActions =
+    SearchMovies
+    | SearchMoviesFailure
+    | SearchMoviesSuccess;
