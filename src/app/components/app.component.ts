205159d62db4f78daee00a0b3b3dfa8c57eb160d
Doug Day <dday@mastercontrol.com>
2019-05-14 09:35:31 -0600

    Cleanup movie details display

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, HostListener } from "@angular/core";
-import { Store } from "@ngrx/store";
-import { Observable } from "rxjs";
+import { select, Store } from "@ngrx/store";
+import { Observable, of, timer } from "rxjs";
+import { mapTo, switchMap } from "rxjs/operators";
 import { SearchResult } from "../models/themoviedb";
-import { getSelectedMovie } from "../movie.selectors";
 import { SelectMovie } from "../movie.actions";
+import { getSelectedMovie } from "../movie.selectors";
 
 @Component({
     selector: "app-root",
@@ -12,15 +13,30 @@ import { SelectMovie } from "../movie.actions";
 })
 export class AppComponent {
     selectedMovie$: Observable<SearchResult>;
+    selectedMovieDetails$: Observable<SearchResult>;
 
     constructor(
-        private store: Store<any>,
+        private store$: Store<any>,
     ) {
-        this.selectedMovie$ = this.store.select(getSelectedMovie);
+        this.selectedMovie$ = this.store$.pipe(select(getSelectedMovie));
+
+        // Setup an observable for displaying the movie details that
+        // waits for 1 second before being deselected (to account for
+        // the animation delay)
+        this.selectedMovieDetails$ = this.store$.pipe(
+            select(getSelectedMovie),
+            switchMap(movie => {
+                if (!movie) {
+                    // When deselected, wait 1 second before emitting the value
+                    return timer(1000).pipe(mapTo(movie));
+                }
+                return of(movie);
+            })
+        );
     }
 
     deselect() {
-        this.store.dispatch(new SelectMovie(null));
+        this.store$.dispatch(new SelectMovie(null));
     }
 
     handleClose() {
