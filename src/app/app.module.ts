d564f141756153993fa0e5c66bde1161c10f45ed
Doug Day <dday@mastercontrol.com>
2019-05-13 19:01:50 -0600

    Removed unused files from a previous training

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./components/app.component";
 import { MovieDetailsComponent } from "./components/movie-details/movie-details.component";
 import { MovieListComponent } from "./components/movie-list/movie-list.component";
-import { AuthService } from "./services/auth.service";
 import { MovieService } from "./services/movie.service";
 
 @NgModule({
@@ -18,7 +17,6 @@ import { MovieService } from "./services/movie.service";
         HttpClientModule,
     ],
     providers: [
-        AuthService,
         MovieService,
     ],
     bootstrap: [AppComponent]
